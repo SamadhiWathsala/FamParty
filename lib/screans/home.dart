f0918ed418d485a07b78016b8b74dc09aea330fe
Samadhi Wathsala <samadhiwathsala96@gmail.com>
2020-10-19 20:01:57 +0530

    initial commit

---
+++ lib/screans/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Text("Welcome to the FamParty.Here is the very begining of the project",),
+      ),
+    );
+  }
+}
